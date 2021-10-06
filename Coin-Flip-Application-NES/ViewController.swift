35535ecfb37965a44a586efdf38449e95b49ed0f
NES <nspry1997@gmail.com>
2021-10-06 16:33:39 -0400

    Initial Commit

---
+++ Coin-Flip-Application-NES/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coin-Flip-Application-NES
+//
+//  Created by Nathaniel Spry on 10/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
