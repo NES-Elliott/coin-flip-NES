6bd5ed14079a99f13d7243eea157e509c832b242
NES <nspry1997@gmail.com>
2021-10-06 17:52:14 -0400

    coin flip assignment

--- Coin-Flip-Application-NES/ViewController.swift
+++ Coin-Flip-Application-NES/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var images = [#imageLiteral(resourceName: "Heads"), #imageLiteral(resourceName: "Tails")]
+
+    @IBOutlet weak var coinImage: UIImageView!
+    @IBOutlet weak var coinTextOutput: UILabel!
+    @IBAction func coinFlipButton(_ sender: UIButton) {
+        let randomIndex = Int.random(in: 0...1)
+        coinImage.image = images[randomIndex]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
